8a48ebecc09e52673e0ba4a06bb5b89ced55b4b6
habeeb <bidehassan@gmail.com>
2024-05-10 20:56:07 +0000

    betty issues

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """ This method finds a peak in an integers list"""
 def find_peak(list_of_integers):
-	"""
-	Finds a peak in a list of unsorted integers.
+        """
+        Finds a peak in a list of unsorted integers.
 
-	Args:
-		list_of_integers (list): A list of unsorted integers.
+        Args:
+                list_of_integers (list): A list of unsorted integers.
 
-	Returns:
-		int: The peak element.
-	"""
-	if not list_of_integers:
-		return None
+        Returns:
+                int: The peak element.
+        """
+        if not list_of_integers:
+                return None
 
-	left, right = 0, len(list_of_integers) - 1
+        left, right = 0, len(list_of_integers) - 1
 
-	while left < right:
-		mid = (left + right) // 2
-		if list_of_integers[mid] < list_of_integers[mid + 1]:
-			left = mid + 1
-		else:
-			right = mid
-	return list_of_integers[left]
+        while left < right:
+                mid = (left + right) // 2
+                if list_of_integers[mid] < list_of_integers[mid + 1]:
+                        left = mid + 1
+                else:
+                        right = mid
+
+        return list_of_integers[left]
