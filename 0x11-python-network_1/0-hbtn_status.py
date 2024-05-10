d2b1defb39eca80c3867c3782438930c4413e6a4
habeeb <bidehassan@gmail.com>
2024-05-10 22:44:59 +0000

    version 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 @!/usr/bin/python3
 """ a Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urlib.request
+
+
 if __name__ == "__main__":
     import urllib.request as request
     with request.urlopen('https://alx-intranet.hbtn.io/status') as r:
-        html = r.read()
+        body = r.read()
         print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
