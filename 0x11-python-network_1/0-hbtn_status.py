0a5dc1d546acb1509c62c39feb29dc7b984ea83d
habeeb <bidehassan@gmail.com>
2024-05-10 23:39:34 +0000

    version 4

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
-@!/usr/bin/python3
-""" a Python script that fetches https://alx-intranet.hbtn.io/status"""
-
+#!/usr/bin/python3
+"""Fetchesa given link"""
+import urllib.request
 
 
 if __name__ == "__main__":
-    import urllib.request as request
-    with request.urlopen('https://alx-intranet.hbtn.io/status') as r:
-        body = r.read()
-        print('Body response:')
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,24 +1,13 @@
-@!/usr/bin/python3
-"""  a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id"""
-
-import urllib.request
+#!/usr/bin/python3
+"""Displays the X-Request-Id header variable of a request
+"""
 import sys
+import urllib.request
 
-# Check if a URL is provided as a command-line argument
-if len(sys.argv) != 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-try:
-    # Send a request to the URL
-    with urllib.request.urlopen(url) as response:
-        # Get the value of the X-Request-Id variable from the response headers
-        request_id = response.headers.get('X-Request-Id')
-        if request_id:
-            print("X-Request-Id:", request_id)
-        else:
-            print("X-Request-Id header not found in the response.")
-except urllib.error.URLError as e:
-    print("Error occurred while fetching the URL:", e)
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
